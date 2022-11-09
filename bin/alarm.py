bafcc7f963a1216b3c5f009eabf6ecee1e3567a3
Andrea Bravetti <andreabravetti@gmail.com>
2022-11-09 13:11:25 +0100

    Stop motion to take a photo

--- bin/alarm.py
+++ bin/alarm.py
@@ -111,6 +111,7 @@ while True:
                             try:
                                 if active_motion:
                                     subprocess.run(['systemctl', 'stop', 'motion']).check_returncode()
+                                    time.sleep(1)
                                 result = subprocess.run(["fswebcam", "-r", "2592x1944", photo_name])
                                 if result.returncode == 0:
                                     photo_sub = "Photo taken on %s saved in %s" % (datetime.now().strftime("%Y/%m/%d, %H:%M:%S"), photo_name)
